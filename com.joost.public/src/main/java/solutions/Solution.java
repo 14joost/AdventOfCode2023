1b47fe43fc6ce11824d2f1698b9dcf24ccd55c51
14joost <j.w.m.sessink@student.utwente.nl>
2023-11-29 23:02:23 +0100

    Make day a field of the Solution class to function as an id

--- com.joost.public/src/main/java/solutions/Solution.java
+++ com.joost.public/src/main/java/solutions/Solution.java
@@ -1,5 +1,7 @@
 package solutions;
 
+import lombok.Getter;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -7,16 +9,19 @@ import java.io.IOException;
 
 import static java.lang.String.format;
 
+@Getter
 public abstract class Solution {
   private static final String PATH_PREFIX =
       System.getProperty("user.dir") + "\\com.joost.public\\src\\main\\resources\\input\\";
   private static final String INPUT_PREFIX = "input_";
   private static final String EXAMPLE_INPUT_PREFIX = "example_input_";
   private static final String FILE_EXTENSION = ".txt";
+  private final int day;
   private final BufferedReader inputReader;
   private final BufferedReader exampleInputReader;
 
   public Solution(int day) throws FileNotFoundException {
+    this.day = day;
     this.inputReader =
         new BufferedReader(
             new FileReader(format("%s%s%s%s", PATH_PREFIX, INPUT_PREFIX, day, FILE_EXTENSION)));
